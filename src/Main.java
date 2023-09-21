df84105cb2f34a2258c14cde9564173b5268d4e1
Eemeli23002 <eemeli23002@student.hamk.fi>
2023-09-21 11:55:53 +0300

    Jakolaskun, summan ja erotuksen alustus

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 String tekija = "Eemeli";
 		
-int luku1=7;
+int luku1=10;
 int luku2=5;
 int tulo = luku1 * luku2;
+int jako = luku1 / luku2;
+int erotus = luku1 - luku2;
+int summa = luku1 + luku2;
 	System.out.println("Hei olen tulostin ohjelma");
 System.out.println("Ohjelman tekijä: " + tekija);
 
@@ -15,6 +18,7 @@ System.out.println("Muuttujan 1 arvo on: "+ luku1);
 System.out.println("Muuttujan 2 arvo on: "+ luku2);
 System.out.println(tulo);
 
+System.out.println(jako);
 	}
 
 }
