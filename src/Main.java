e5e13ca9b9c4c195d8987d4f3c0e042b86fcb52c
Eemeli23002 <eemeli23002@student.hamk.fi>
2023-09-21 11:53:02 +0300

    Tulon alustus

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ String tekija = "Eemeli";
 		
 int luku1=7;
 int luku2=5;
+int tulo = luku1 * luku2;
 	System.out.println("Hei olen tulostin ohjelma");
 System.out.println("Ohjelman tekijä: " + tekija);
 
 System.out.println("Muuttujan 1 arvo on: "+ luku1);
 
 System.out.println("Muuttujan 2 arvo on: "+ luku2);
+
+
 	}
 
 }
