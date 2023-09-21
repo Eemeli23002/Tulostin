dea288ebf1b45b9283afa66a8e19f454e5740d17
Eemeli23002 <eemeli23002@student.hamk.fi>
2023-09-21 11:51:52 +0300

    Muuttujien alustus ja tulostus

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 String tekija = "Eemeli";
 		
+int luku1=7;
+int luku2=5;
 	System.out.println("Hei olen tulostin ohjelma");
 System.out.println("Ohjelman tekijä: " + tekija);
 
+System.out.println("Muuttujan 1 arvo on: "+ luku1);
 
+System.out.println("Muuttujan 2 arvo on: "+ luku2);
 	}
 
 }
